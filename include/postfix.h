f61e6c2d1a0c762ac6e2a1b894795960e1722220
Stolbov Yaroslav <stojar2001@gmail.com>
2021-01-19 14:43:46 +0300

    rpn initial

--- include/postfix.h
+++ include/postfix.h
@@ -1,8 +1,8 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 Stolbov Yaroslav
 #ifndef INCLUDE_POSTFIX_H_
 #define INCLUDE_POSTFIX_H_
 #include <string>
 
 std::string infix2postfix(std::string infix);
-
+int getP(char token);
 #endif  // INCLUDE_POSTFIX_H_
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -1,7 +1,60 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 Stolbov Yaroslav
 #include "MyStack.h"
+#include <stack>
 #include "postfix.h"
-
+#include <string>
 std::string infix2postfix(std::string infix) {
-  return infix;
+    std::string current;
+    std::stack <char> st;
+    int priority;
+    for (int i = 0; i < infix.length(); i++) {
+        priority = getP(infix[i]);
+        if (priority == 0)
+            current += infix[i];
+        if (priority == 1)
+            st.push(infix[i]);
+        if (priority > 1){
+            //current += ' ';
+            while (!st.empty()) {
+                if (getP(st.top()) >= priority) {
+                    current = current + st.top();
+                    st.pop();
+                }
+                else {
+                    break;
+                }
+            }
+            st.push(infix[i]);
+        }
+        if (priority == -1) {
+            while (getP(st.top()) != 1) {
+                current += ' ';
+                current += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+    }
+    current += ' ';
+    while (!st.empty()) {
+        current += st.top();
+        st.pop();
+        current += ' ';
+    }
+    for (int i = 0; i < current.length(); i++)
+        if (current[i] == ' ' && current[i + 1] == ' ') {current.erase(i, 1); i = (i - 1);}
+  return current;
+}
+
+int getP(char token) {
+    if (token == '*' || token == '/')
+        return 3;
+    else if (token == '+' || token == '-')
+        return 2;
+    else if (token == '(')
+        return 1;
+    else if (token == ')')
+        return -1;
+    else
+        return 0;
 }
